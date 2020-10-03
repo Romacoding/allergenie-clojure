4026d0972c6408adf5e1564bf0b894eb81cfd4e8
Roman Ostash <roman.ostash@stots.edu>
2020-10-03 17:26:07 -0400

    clean up

--- src/allergenie/core.clj
+++ src/allergenie/core.clj
@@ -6,45 +6,43 @@
             [config.core :refer [env]]
             [clj-http.client :as client]
             [clojure.data.json :as json]
-            [clojure.pprint]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as str]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.session-timeout :refer [wrap-idle-session-timeout]]
-            [clojure.string :as str])
+            )
   (:gen-class))
 
 (def pollen-info (atom {}))
 (def zip-info (atom []))
 (def air-info (atom []))
 (def weather-info(atom {}))
-(def input-info(atom 0))
 
 (defc main-page []
   [:div {:class "main-page-div"}
    [:h1 {:style {:text-align "center"}} "AllerGenie"]
+   [:div
+    [:a {:href "/" :style {:padding "10px"}} "Home page"]
+    [:a {:href "/json" :style {:padding "10px"}} "JSON response"]
+    [:a {:href "/about" :style {:padding "10px"}} "About page"]]
    [:h3 (str "Your zip code is: " (:zip (first @zip-info)))]
-   [:input {:type      "text"
-            :allow-full-screen true
-            :id        "comment"
-            :class     ["input_active"]
-            :style     {:background-color "#EEE"
-                        :margin-left      42}
-            :on-change (fn [e]
-                         (swap! input-info assoc (.. e -target -value))
-                         (println (.. e -target -value))
-                         (println @input-info))}]
-   [:p (str "Air quality info: " (:zip (first @air-info)))]
+   [:h3 "Air quality info"]
+   [:p (str "Pollutant: " (:Name (first @air-info)))]
+   [:p (str "Air Quality Index: " (:AQI (first @air-info)))]
+   [:p (str "Pollutant: " (:Name (nth @air-info 1)))]
+   [:p (str "Air Quality Index: " (:AQI (nth @air-info 1)))]
+   [:h3 "Pollen info"]
    [:p (str "Pollen index for today is: " (:Index @pollen-info))]
-   [:p (str "Weather info: " (:description @weather-info) ". " "Temperature: " (:temperature @weather-info) "°F")]])
+   [:p (str "Main triggers: " (:Name (:Triggers @pollen-info)))]
+   [:h3 "Weather info"]
+   [:p (str (:description @weather-info) ". " "Temperature: " (:temperature @weather-info) "°F")]])
 
 (defc about-page []
   [:div {:class "about-page-div"}
    [:h1 "About the project"]
    [:p "AllerGenie provides pollen and weather forecasts you can use everyday.\n
-Our mission is to improve the quality of life through timely and accurate information intended to assist all allergy sufferers."]])
-
-;; (defn adddata [keyfield valuefield]
-;;   (swap! pollen-info conj {:keyfield keyfield :valuefield valuefield}))
-;; (adddata "zip" "08865")
+Our mission is to improve the quality of life through timely and accurate information intended to assist all allergy sufferers."]
+   [:a {:href "/"} "Home page"]])
 
 (defn return-json [req]
   {:status  200
@@ -55,10 +53,10 @@ Our mission is to improve the quality of life through timely and accurate inform
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (->>
-             (clojure.pprint/pprint req)
+             (pprint req)
              (str "Request Object: " req))})
 
-(defn simple-page [req]
+(defn sample-page []
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "Hello World"})
@@ -67,14 +65,14 @@ Our mission is to improve the quality of life through timely and accurate inform
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (->
-             (clojure.pprint/pprint req)
+             (pprint req)
              (str "Pollen " (:name (:params req))))})
 
 (defroutes app
   (GET "/" [] (render-static-markup (main-page)))
   (GET "/about" [] (render-static-markup (about-page)))
   (GET "/request" [] request-example)
-  (GET "/page" [] simple-page)
+  (GET "/page" [] sample-page)
   (GET "/pollen" [] pollen-page)
   (GET "/json" [] return-json)
   (not-found "<h1>Sorry, page not found!</h1>"))
@@ -84,7 +82,6 @@ Our mission is to improve the quality of life through timely and accurate inform
 ;         fstr (str/replace string -re "%s")
 ;         fargs (map #(read-string (second %)) (re-seq -re string))]
 ;     `(format ~fstr ~@fargs)))
-
 
 (defn get-zip []
   (let [resp (client/get "http://ip-api.com/json/?fields=zip")
@@ -99,31 +96,23 @@ Our mission is to improve the quality of life through timely and accurate inform
   (let [resp (client/get (str "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" (:zip (first @zip-info)) "&API_KEY=" (:airkey env)))
         body (json/read-str (resp :body)
                             :key-fn keyword)]
-    ; (reduce (fn [acc {:keys [Name] :as it}]
-    ;           (assoc acc Name it)) {} (:data body)) 
-    ;(clojure.pprint/pprint body)
     (println "Air pollution information")
     ;(println (str (:ParameterName (nth body 1)) " index is: " (:AQI (nth body 1)) ", " (:Name (:Category (nth body 1)))))
     (println (str (:ParameterName (first body)) " index is: " (:AQI (first body)) ", " (:Name (:Category (first body)))))
-    (swap! air-info conj {:AQI (:AQI (first body)) :Name (:ParameterName (first body))})))
+    (swap! air-info conj {:AQI (:AQI (first body)) :Name (:ParameterName (first body))})
+    (swap! air-info conj {:AQI (:AQI (nth body 1)) :Name (:ParameterName (nth body 1))})))
 
 (get-air)
-
-;; (format "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=%s%s%s" zip-code small api-key))
 
 (defn get-weather []
   (let [resp (client/get (str "http://api.openweathermap.org/data/2.5/weather?zip=" (:zip (first @zip-info)) ",us&appid=" (:weatherkey env)))
         body (json/read-str (resp :body)
                             :key-fn keyword)]
-    ; (reduce (fn [acc {:keys [Name] :as it}]
-    ;           (assoc acc Name it)) {} (:data body)) 
-    ; (clojure.pprint/pprint body)
     (swap! weather-info assoc :description (str/capitalize (:description (first
                                                                                (:weather body))))
            :temperature (int (Math/floor (- (* 1.8 (:temp (:main body))) 459.67))))
     (println "Weather information")
-    (println (str/capitalize (:description (first
-                                                       (:weather body)))))
+    (println (str/capitalize (:description (first (:weather body)))))
     (println (str "Temperature: " (int (Math/floor (- (* 1.8 (:temp (:main body))) 459.67))) " F"))))
 
 (get-weather)
@@ -133,9 +122,10 @@ Our mission is to improve the quality of life through timely and accurate inform
         body (json/read-str (resp :body)
                             :key-fn keyword)]
     (swap! pollen-info assoc :Index (:Index (nth (:periods (:Location body)) 1)))
+    (swap! pollen-info assoc :Triggers (first (:Triggers (nth (:periods (:Location body)) 1))))
     (println "Pollen information")
-    ;(println @pollen-info)
-    (println (str "Current polen index: " (:Index (nth (:periods (:Location body)) 1))))))
+    (println (str "Current polen index: " (:Index (nth (:periods (:Location body)) 1))))
+  ))
 
 (get-pollen)
 
@@ -146,5 +136,5 @@ Our mission is to improve the quality of life through timely and accurate inform
 
 (defn -main
   [& args]
-  (println (str "Running webserver at http:/127.0.0.1:" (:port env)  "/"))
+  #_(println (str "Running webserver at http:/127.0.0.1:" (:port env)  "/"))
   (run-jetty (wrap-defaults app site-defaults) {:port (:port env)}))
