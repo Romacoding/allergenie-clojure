0ed27c5fe150197356089bd833c9932834241294
Roman Ostash <roman.ostash@stots.edu>
2020-10-11 08:39:52 -0400

    Bulma UI added

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.8.0"]
-                 [rum "0.11.4"]
                  [compojure "1.6.2"]
                  [yogthos/config "1.1.7"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-session-timeout "0.2.0"]
                  [clj-http "3.10.2"]
-                 [org.clojure/data.json "1.0.0"]]
+                 [org.clojure/data.json "1.0.0"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot allergenie.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- src/allergenie/core.clj
+++ src/allergenie/core.clj
@@ -2,9 +2,9 @@
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [not-found]]
-            [rum.core :refer [defc render-static-markup]]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]
             [config.core :refer [env]]
-            [clojure.data.json :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [allergenie.util :refer [get-pollen calc-wind-dir calc-pollen-level get-air get-weather]])
   (:gen-class))
@@ -24,44 +24,72 @@
 
 (swap! weather-info assoc :wind-dir (calc-wind-dir (num (:wind-deg @weather-info))))
 
-(defc main-page []
-  [:body {:style {:background-color "#fef9c7"}}
-   [:div {:class "main-page-div"}
-    [:h1 {:style {:text-align "center"}} "AllerGenie"]
-    [:div
-     [:a {:href "/" :style {:padding "10px"}} "Home page"]
-     [:a {:href "/json" :style {:padding "10px"}} "JSON response"]
-     [:a {:href "/about" :style {:padding "10px"}} "About page"]]
-    [:h3 {:style {:text-align "center"}} "Request form"
-     [:form {:style {:padding "5px"} :action "/pollen"}
-      [:label {:style {:padding "5px"} :for "zip"} "Change your location"] [:br]
-      [:input {:name "zip" :type "text" :id "zip" :placeholder "enter your zip code" :required ""}] [:br]
-      [:input {:style {:padding "5px"} :type "submit" :value "Submit"}]]]
-    [:h3 (str "Information for: " (:location @pollen-info) " " (:zip @zip-info))]
-    [:h3 "Air quality info"]
-    [:p (str "Pollutant: " (:name (first @air-info)))]
-    [:p (str "Air Quality Index: " (:aqi (first @air-info)) ", " (:level (first @air-info)))]
-    [:p (str "Pollutant: " (:name (nth @air-info 1)))]
-    [:p (str "Air Quality Index: " (:aqi (nth @air-info 1)) ", " (:level (nth @air-info 1)))]
-    [:h3 "Pollen info"]
-    [:p (str "Pollen index for today is: " (:index @pollen-info) ", " (:level @pollen-info))]
-    [:p (str "Main triggers: " (:triggers @pollen-info))]
-    [:h3 "Weather info"]
-    [:p
-     [:img {:src (str "http://openweathermap.org/img/wn/" (:icon @weather-info) "@2x.png")}]]
-    [:p (str (:description @weather-info) ". " "Temperature: " (:temperature @weather-info) "°F")]
-    [:p (str "Humidity: ") (:humidity @weather-info) "%"]
-    [:p (str "Wind speed: ") (:wind-speed @weather-info) "Mph"]
-    [:p (str "Wind direction: ") (:wind-dir @weather-info)]
-    ]]
-  )
+(defn main-page [req]
+  (html5 {:lang "en"}
+         [:head [:title "AllerGenie"]
+          (include-css "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css")
+          [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]]
+         [:body
+          [:div {:class "container"}
+           [:h1 {:class "title is-1 has-text-centered"} "AllerGenie"]
 
-(defc about-page []
-  [:div {:class "about-page-div"}
-   [:h1 "About the project"]
-   [:p "AllerGenie provides pollen and weather forecasts you can use everyday.\n
-Our mission is to improve the quality of life through timely and accurate information intended to assist all allergy sufferers."]
-   [:a {:href "/"} "Home page"]])
+           [:nav {:class "level"}
+            [:p {:class "level-item has-text-centered"}
+             [:a {:class "link is-info" :href "/"} "Home"]]
+            [:p {:class "level-item has-text-centered"}
+             [:a {:class "link is-info" :href "/login"} "Login"]]
+            [:p {:class "level-item has-text-centered"}
+             [:img {:src "/img/genie.svg" :alt "AllegGenie logo" :style {:size "20px"}}]]
+            [:p {:class "level-item has-text-centered"}
+             [:a {:class "link is-info" :href "/glossary"} "Glossary"]]
+            [:p {:class "level-item has-text-centered"}
+             [:a {:class "link is-info" :href "/about"} "About"]]]
+
+           [:div {:class "box m-4"}
+            [:form {:action "/pollen"}
+             [:label {:for "zip"} "Change your location"] [:br]
+             [:input {:class "input m-4" :name "zip" :type "tel" :id "zip" :placeholder "enter your zip code" :required "" }]
+             [:input {:class "button is-info m-4" :type "submit" :value "Submit"}]]]
+
+           [:h3 {:class "title is-3 has-text-centered m-4"} (str "Information for: " (:location @pollen-info) " " (:zip @zip-info))]
+
+           [:div {:class "box columns is-8"}
+            [:div {:class "column card m-4"}
+             [:h3 {:class "title is-3"} "Air quality info"]
+             [:p (str "Pollutant: " (:name (first @air-info)))]
+             [:p (str "Air Quality Index: " (:aqi (first @air-info)) ", " (:level (first @air-info)))]
+             [:p (str "Pollutant: " (:name (nth @air-info 1)))]
+             [:p (str "Air Quality Index: " (:aqi (nth @air-info 1)) ", " (:level (nth @air-info 1)))]]
+
+            [:div {:class "column card m-4"}
+             [:h3 {:class "title is-3"} "Pollen info"]
+             [:p (str "Pollen index for today is: " (:index @pollen-info) ", " (:level @pollen-info))]
+             [:progress {:class "progress is-success" :value (str (:index @pollen-info)) :max "12"} (:index @pollen-info)]
+             [:p (str "Main allergens: " (:triggers @pollen-info))]]
+
+            [:div {:class "column card m-4"}
+             [:h3 {:class "title is-3"} "Weather info"]
+             [:p
+              [:img {:src (str "http://openweathermap.org/img/wn/" (:icon @weather-info) "@2x.png")}]]
+             [:p (str (:description @weather-info))]
+             [:p (str "Temperature: " (:temperature @weather-info) "°F")]
+             [:p (str "Humidity: ") (:humidity @weather-info) "%"]
+             [:p (str "Wind speed: ") (:wind-speed @weather-info) "Mph"]
+             [:p (str "Wind direction: ") (:wind-dir @weather-info)]]]]
+          [:footer {:class "footer"}
+           [:div {:class "content has-text-centered"}
+            [:p [:strong "AllerGenie"] " by " [:a {:href "https://ostash.dev"} "© Roman Ostash 2020"]]]]]))
+
+(defn about-page
+  [req]
+  (html5 {:lang "en"}
+         [:head]
+         [:body
+          [:div {:class "about-page-div"}
+           [:h1 "About the project"]
+           [:p "AllerGenie provides pollen and weather forecasts you can use everyday.\n 
+                Our mission is to improve the quality of life through timely and accurate information intended to assist all allergy sufferers."]
+           [:a {:href "/"} "Home page"]]]))
 
 (defn pollen-page [req]
   (reset! zip-info {:zip (:zip (:params req))})
@@ -70,14 +98,13 @@ Our mission is to improve the quality of life through timely and accurate inform
   (reset! pollen-info (get-pollen (:zip @zip-info)))
   (swap! pollen-info assoc :level (calc-pollen-level (:index @pollen-info)))
   (swap! weather-info assoc :wind-dir (calc-wind-dir (num (:wind-deg @weather-info))))
-  
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body (str "Zip updated to " (:zip @zip-info) "<br/><a href='/'>Home page</>")})
 
 (defroutes app
-  (GET "/" [] (render-static-markup (main-page)))
-  (GET "/about" [] (render-static-markup (about-page)))
+  (GET "/" [] main-page)
+  (GET "/about" [] about-page)
   (GET "/pollen" [] pollen-page)
   (not-found "<h1>Sorry, page not found!</h1>"))
 
