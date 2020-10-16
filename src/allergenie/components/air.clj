444dbd4e191e34746aadc8a9a9562357c7d27765
Roman Ostash <roman.ostash@stots.edu>
2020-10-15 22:02:39 -0400

    Air component

--- src/allergenie/components/air.clj
+++ src/allergenie/components/air.clj
@@ -5,7 +5,7 @@
   [:div {:class "column card m-4"}
    [:h3 {:class "title is-3 has-text-centered"} "Air Quality"]
    [:p {:class "is-small m-2"} "The air you breathe matters. Besides pollen, there's all sorts of other stuff in the air that can have an effect on your breathing and allergy symptoms."]
-   [:div {:class "box"}]
+   [:div ]
    (let [pollutans state]
      (for [pollutant pollutans]
        [:div {:class "m-2"}
