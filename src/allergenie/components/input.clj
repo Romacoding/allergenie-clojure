7e948ac65800a7d42625c4b52aa6de950b9b8e80
Roman Ostash <roman.ostash@stots.edu>
2020-10-17 19:10:24 -0400

    UI fixes. Input

--- src/allergenie/components/input.clj
+++ src/allergenie/components/input.clj
@@ -5,7 +5,7 @@
    [:div {:class "column is-half"}
     [:form {:action "/info"}
      [:div {:class "field"}
-      [:label {:for "zip" :class "ml-5"} "Please, provide us your location in the US"]
+      [:label {:for "zip" :class "ml-5"} "Provide us your location in the US"]
       [:div {:class "control m-4"}
        [:input {:class "input m-2" :name "zip" :type "tel" :id "zip" :placeholder "enter your zip code" :required ""}]
        [:input {:class "button is-info m-2" :type "submit" :value "Submit"}]]]]]])
